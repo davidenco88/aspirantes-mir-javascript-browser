2c8adf42ae231bc0a40480a6a3a488916490d60c
David Sarria <david.sarriav@gmail.com>
2023-03-28 22:28:13 +0200

    Solución funcional sin bonus

--- js-brow-2/script.js
+++ js-brow-2/script.js
@@ -1,30 +1,85 @@
-let addButton = document.querySelector('#add-button');
-let taskList = document.querySelector('#task-list');
-let inputNewTask = document.querySelector('#new-task');
+let tasks = [];
+let id = 1;
 
-let taskListArray = [];
-let idTask = 0;
+const addButton = document.querySelector('#add-button');
+const deleteButton = document.querySelector('#delete-button');
+const taskList = document.querySelector('#task-list');
 
+function handleCheckTask (event, task) {
+    const input = event.currentTarget.querySelector('input');
+    const span = event.currentTarget.querySelector('span');
+    const isChecked = input.getAttribute('checked');
 
-const handleSubmit = () => {
-    let newTask = {};
-    let newItemList = document.createElement('li');
-    
-    newTask.id = taskListArray.length + 1;
-    newTask.title = inputNewTask.value;
-    newTask.complete = false;
-    
-    taskListArray[taskListArray.length] = newTask;
-    newItemList.innerHTML = newTask.title;
-    taskList.appendChild(newItemList);
+    if (isChecked) {
+        input.removeAttribute('checked');
+        span.classList.remove('is-complete');
+        tasks = tasks.map(item => item.id === task.id ? {...item, complete: false} : item);
+    } else {
+        input.setAttribute('checked', true);
+        span.classList.add('is-complete');
+        tasks = tasks.map(item => item.id === task.id ? {...item, complete: true} : item);
+    }
+
+    console.log(tasks);
 }
 
-const handleCheckTask = () => {
+function renderTask(task) {
     
+    const taskCheckbox = document.createElement('input');
+    const taskItem = document.createElement('li');
+
+    taskCheckbox.setAttribute('type', 'checkbox');
+
+    const taskLabel = document.createElement('span');
+    taskLabel.textContent = task.title
+
+    taskItem.appendChild(taskCheckbox);
+    taskItem.appendChild(taskLabel);
+    taskItem.addEventListener('click', (event) => {
+        handleCheckTask( event, task);
+    })
+    
+    taskList.appendChild(taskItem);
 }
 
-const handleDelete = () => {
+
+function handleSubmit (event) {
     
+    event.preventDefault();
+
+    const textInput = document.querySelector('#new-task');
+
+    const task = {
+        id,
+        title: textInput.value,
+        complete: false
+    }
+
+    tasks.push(task);
+    id++;
+    textInput.value = '';
+
+    renderTask(task);
 }
 
-addButton.onclick = handleSubmit;
+
+
+function handleDelete (event) {
+    event.preventDefault();
+    const list = taskList.querySelectorAll('li');
+    list.forEach(element => {
+        const isChecked = element.children[0].getAttribute('checked');
+
+        isChecked ? element.remove() : null;
+    })
+}
+
+//addButton.onclick = handleSubmit;
+
+addButton.addEventListener('click', (event) => {
+    handleSubmit(event);
+})
+
+deleteButton.addEventListener('click', (event) => {
+    handleDelete(event);
+})
